401d8a2767c0fb882843da0614507a85e301f189
Darwin Libioulle <Libioulle.Darwin@hls.be>
2026-01-07 17:17:29 +0100

    Adding changes for scripting

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # move_folder_to_folder
 Powershell Script
 
-#By Pass 
+# By Pass 
  powershell -ExecutionPolicy Bypass -File "Your Path F:\..\file.ps1"
+
+# Cast to Intunne
+ Use the software by intunne
+ drag and drop
+
+# Check
+ Chek on the laptop if it is insttall
+ 
  
--- achat_push.ps1
+++ achat_push.ps1
@@ -1,7 +1,23 @@
 
 # Define source and destination paths
-$source = "G:/Test"
-$destination = "C:\Achats64"
+$source = "\\local\...\softwareFolder"
+$destination = "S:\softwareFolder"
 
-# Move the folder
-Move-Item -Path $source -Destination $destination
+try {
+    # Check if source exists
+    if (Test-Path $source) {
+        # Check if destination already exists
+        if (Test-Path $destination) {
+            Write-Host "Error : Destination folder already exists: $destination"
+        } else {
+            # Copy folder and contents
+            Copy-Item -Path $source -Destination $destination -Recurse -Force
+            Write-Host "OK : Folder copied successfully from $source to $destination"
+        }
+    } else {
+        Write-Host "Error :  Source folder does not exist: $source"
+    }
+}
+catch {
+    Write-Host "Error : An error occurred: $($_.Exception.Message)"
+}
